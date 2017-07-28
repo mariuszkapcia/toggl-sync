d9208921c83ac8465d587773061c27b410360dfe
Mariusz Kapcia <m.kapcia@nomtek.com>
2017-07-28 22:04:10 +0200

    Upgrading gems version to handle console warnings

--- toggl_sync.rb
+++ toggl_sync.rb
@@ -4,15 +4,15 @@ $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/faraday-0.11.0/lib")
 $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/multipart-post-2.0.0/lib")
 $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/oj-2.18.1/lib")
 $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/logger-1.2.8/lib")
-$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/awesome_print-1.7.0/lib")
+$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/awesome_print-1.8.0/lib")
 
 # NOTE Loading JiraRuby and dependencies
 $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/jira-ruby-1.2.0/lib")
-$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/activesupport-5.0.1/lib")
-$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/concurrent-ruby-1.0.4/lib")
-$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/i18n-0.7.0/lib")
+$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/activesupport-5.0.2/lib")
+$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/concurrent-ruby-1.0.5/lib")
+$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/i18n-0.8.6/lib")
 $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/tzinfo-1.2.2/lib")
-$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/thread_safe-0.3.5/lib")
+$LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/thread_safe-0.3.6/lib")
 $LOAD_PATH.unshift("#{Dir.pwd}/vendor/bundle/gems/oauth-0.5.1/lib")
 
 # NOTE Loading own libs
